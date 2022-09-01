68225c314e9162d31f58446231033e035d20b1c4
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-01 17:36:40 +0530

    created SimpleInterestCalculator method

--- src/main/java/com/aditya/SimpleInterestCalculator.java
+++ src/main/java/com/aditya/SimpleInterestCalculator.java
@@ -5,5 +5,20 @@
  */
 package com.aditya;
 
+import java.util.Scanner;
+
 public class SimpleInterestCalculator {
+
+    /**
+     * This method calculates the simple interest based on the following formula.
+     *
+     * @param principleAmount   the principal amount in rupees (₹).
+     * @param interestRate      the interest rate.
+     * @param timePeriodInYears the time period for the loan.
+     * @return the calculated simple interest.
+     */
+    public float calculateSimpleInterest(float principleAmount, float interestRate, float timePeriodInYears) {
+        return (principleAmount * interestRate * timePeriodInYears / 100);
+    }
 }
+
