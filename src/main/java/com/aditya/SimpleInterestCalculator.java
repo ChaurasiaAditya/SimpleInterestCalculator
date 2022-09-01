b354d65120840d6e31303913c3f7be4dd658c6c7
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-01 17:37:20 +0530

    call the SimpleInterestCalculator method and print the result

--- src/main/java/com/aditya/SimpleInterestCalculator.java
+++ src/main/java/com/aditya/SimpleInterestCalculator.java
@@ -8,6 +8,35 @@ package com.aditya;
 import java.util.Scanner;
 
 public class SimpleInterestCalculator {
+    public static void main(String[] args) {
+        // initialize the Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // prompt the user to enter the principal amount
+        System.out.println("Please enter the principle amount in rupees: ₹");
+        // store the principal amount in a variable
+        float principleAmount = scanner.nextFloat();
+
+        // prompt the user to enter the rate of interest
+        System.out.println("Please enter the rate of interest: ");
+        // store the rate of interest in a variable
+        float rateOfInterest = scanner.nextFloat();
+
+        // prompt the user to enter the time period
+        System.out.println("Please enter the time period in years: ");
+        // store the time period in a variable
+        float timePeriod = scanner.nextFloat();
+
+        // call the calculateSimpleInterest method and store the result in a variable
+        SimpleInterestCalculator simpleInterestCalculator = new SimpleInterestCalculator();
+        float simpleInterest = simpleInterestCalculator.calculateSimpleInterest(principleAmount, rateOfInterest, timePeriod);
+
+        // print the result
+        System.out.println("The calculated simple interest is ₹ " + simpleInterest);
+
+        // close the scanner
+        scanner.close();
+    }
 
     /**
      * This method calculates the simple interest based on the following formula.
